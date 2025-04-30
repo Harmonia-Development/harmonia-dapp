85ab4848a0287e266089eeb2cea0896ee9646122
Joel Vargas <vermudo.com@gmail.com>
2025-04-30 12:18:13 -0600

    feat: implement custom hooks for Deposit, Rebalance, Swap, Transaction History, and Transfer functionalities in treasury management

---
+++ apps/webapp/components/treasury/hooks/useTransferFunds.hook.ts
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  TransferFundsFormValues,
+  transferFundsSchema,
+} from "../schema/transfer-funds-modal.schema";
+
+interface TransferFundsFormProps {
+  onClose: () => void;
+}
+
+export const useTransferFunds = ({ onClose }: TransferFundsFormProps) => {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+
+  const form = useForm<TransferFundsFormValues>({
+    resolver: zodResolver(transferFundsSchema),
+    defaultValues: {
+      asset: "xlm",
+      amount: undefined,
+      destination: "",
+      memo: "",
+    },
+  });
+
+  const onSubmit = async (data: TransferFundsFormValues) => {
+    setIsSubmitting(true);
+
+    // Simulate API call
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    console.log("Form submitted:", data);
+
+    // Reset form and close modal
+    form.reset();
+    setIsSubmitting(false);
+    onClose();
+  };
+
+  return { form, isSubmitting, onSubmit };
+};
