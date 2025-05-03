0e51f03b7e0297554df74598c7d954a9fc1e603b
Gabito1451 <obiagaziegabriel@gmail.com>
2025-05-03 02:30:24 +0100

    Implement Full Analytics Dashboard for DAO Insights

---
+++ apps/webapp/lib/types/comparative-types.ts
@@ -0,0 +1,14 @@
+export interface PerformanceData {
+  name: string;
+  value: number;
+}
+export interface PerdormData {
+  name: string;
+  current: number;
+  previous: number;
+}
+
+export interface DoaData {
+  month: string;
+  efficiency: number;
+}
